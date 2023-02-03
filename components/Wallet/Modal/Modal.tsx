33095f4f6718b6d533a84c5c43384f7df7696709
NICOL1201 <117902091+NICOL120@users.noreply.github.com>
2023-02-04 01:42:52 +0800

    Add files via upload

--- components/Wallet/Modal/Modal.tsx
+++ components/Wallet/Modal/Modal.tsx
@@ -9,12 +9,10 @@ import {
   ModalOverlay,
   VStack,
 } from '@chakra-ui/react'
-import KeplrConnectButton from './KeplrConnectButton'
-import CosmostationConnectButton from './CosmostationConnectButton'
-import TerraStationConnectButton from './TerraStationConnectButton'
-import LeapConnectButton from './LeapConnectButton'
+import KeplrConnectButton from 'components/Wallet/Modal/KeplrConnectButton'
+import TerraStationConnectButton from 'components/Wallet/Modal/TerraStationConnectButton'
 
-function WalletModal({ isOpenModal, onCloseModal, chainId }) {
+function WalletModal({ isOpenModal, onCloseModal }) {
   return (
     <Modal isOpen={isOpenModal} onClose={onCloseModal}>
       <ModalOverlay />
@@ -23,14 +21,16 @@ function WalletModal({ isOpenModal, onCloseModal, chainId }) {
         <ModalCloseButton />
         <ModalBody>
           <VStack justify="center" align="center" textAlign="center">
-            {chainId === 'phoenix-1' && (
-              <TerraStationConnectButton onCloseModal={onCloseModal} />
-            )}
+            <TerraStationConnectButton onCloseModal={onCloseModal} />
             <KeplrConnectButton onCloseModal={onCloseModal} />
-            <LeapConnectButton onCloseModal={onCloseModal} />
-            <CosmostationConnectButton onCloseModal={onCloseModal} />
           </VStack>
         </ModalBody>
+
+        {/* <ModalFooter >
+            <Button colorScheme='blue' mr={3} onClick={onCloseModal}>
+              Close
+            </Button>
+          </ModalFooter> */}
       </ModalContent>
     </Modal>
   )
