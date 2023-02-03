038824542132323d85ee4483b55e2b42b5b77794
NICOL1201 <117902091+NICOL120@users.noreply.github.com>
2023-02-04 01:01:20 +0800

    Update KeplrConnectButton.tsx

--- components/Wallet/Modal/KeplrConnectButton.tsx
+++ components/Wallet/Modal/KeplrConnectButton.tsx
@@ -1,28 +1,48 @@
-import React, { useCallback } from 'react'
+import React from 'react'
 
-import { Button, HStack, Text } from '@chakra-ui/react'
-import KeplrWalletIcon from 'components/icons/KeplrWalletIcon'
-import useConnectKeplr from 'hooks/useConnectKeplr'
-import { useRecoilValue } from 'recoil'
-import { walletState } from 'state/atoms/walletAtoms'
+import { Button, HStack, Image, Text } from '@chakra-ui/react'
+import { useWallet } from '@terra-money/wallet-provider'
+import { useTerraStation } from 'hooks/useTerraStation'
 
-function KeplrConnectButton({ onCloseModal }) {
-  const { setKeplrAndConnect } = useConnectKeplr()
-  const { chainId, activeWallet, network } = useRecoilValue(walletState)
-
-  const setKeplrMemo = useCallback(() => {
-    setKeplrAndConnect()
-    onCloseModal()
-  }, [activeWallet, chainId, network])
+function TerraStationConnectButton({ onCloseModal }) {
+  const { availableConnections, availableInstallations } = useWallet()
+  const { connectTerraAndCloseModal, filterForStation } =
+    useTerraStation(onCloseModal)
 
   return (
-    <Button variant="wallet" onClick={() => setKeplrMemo()} colorScheme="black">
-      <HStack justify="space-between" width="full">
-        <Text>Keplr Wallet</Text>
-        <KeplrWalletIcon />
-      </HStack>
-    </Button>
+    <>
+      {availableConnections
+        .filter(filterForStation)
+        .map(({ type, identifier, name, icon }) => (
+          <Button
+            variant="wallet"
+            key={identifier}
+            onClick={() => connectTerraAndCloseModal(type, identifier)}
+          >
+            <HStack justify="space-between" width="full">
+              <Text>{name}</Text>
+              <Image boxSize="24px" objectFit="cover" src={icon} alt={name} />
+            </HStack>
+          </Button>
+        ))}
+      {availableInstallations
+        .filter(filterForStation)
+        .map(({ identifier, name, icon, url }) => (
+          <Button
+            colorScheme="black"
+            key={identifier}
+            onClick={() => (window.location.href = url)}
+          >
+            <img
+              src={icon}
+              alt={name}
+              style={{ width: '1em', height: '1em' }}
+            />
+            Install {name} [{identifier}]
+          </Button>
+        ))}
+    </>
   )
 }
 
-export default KeplrConnectButton
+export default TerraStationConnectButton
