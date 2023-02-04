bf95d9cefdd7a6ad5e23dc6538729c4ee2fd531e
NICOL1201 <117902091+NICOL120@users.noreply.github.com>
2023-02-04 23:14:13 +0800

    Add files via upload

--- components/Wallet/Modal/KeplrConnectButton.tsx
+++ components/Wallet/Modal/KeplrConnectButton.tsx
@@ -1,103 +1,28 @@
-import { useDisclosure, Button, HStack, Flex, Spacer, Image, Text } from "@chakra-ui/react";
-import { useWallet, ConnectType } from "@terra-money/wallet-provider";
-import { FC } from "react";
+import React, { useCallback } from 'react'
 
-import ModalWrapper from "./ModalWrapper";
-import TerraIcon from "./TerraIcon";
+import { Button, HStack, Text } from '@chakra-ui/react'
+import KeplrWalletIcon from 'components/icons/KeplrWalletIcon'
+import useConnectKeplr from 'hooks/useConnectKeplr'
+import { useRecoilValue } from 'recoil'
+import { walletState } from 'state/atoms/walletAtoms'
 
-type WalletOptions = {
-  type: string;
-  identifier?: string;
-  name: string;
-  icon: string;
-  isInstalled?: boolean;
-  walletAction: () => void;
-};
+function KeplrConnectButton({ onCloseModal }) {
+  const { setKeplrAndConnect } = useConnectKeplr()
+  const { chainId, activeWallet, network } = useRecoilValue(walletState)
 
-const WalletConnectButton: FC = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const { connect, availableInstallations, availableConnections } = useWallet();
-
-  const wallets: WalletOptions[] = [
-    ...availableConnections
-      .filter(({ type }) => type !== ConnectType.READONLY)
-      .map(({ type, icon, name, identifier }) => ({
-        type,
-        identifier: identifier ?? "",
-        name,
-        icon,
-        isInstalled: true,
-        walletAction: () => {
-          connect(type, identifier);
-        },
-      })),
-    ...availableInstallations
-      .filter(({ type }) => type !== ConnectType.READONLY)
-      .map(({ type, icon, name, url, identifier }) => ({
-        type,
-        identifier,
-        name: "Install " + name,
-        icon,
-        isInstalled: false,
-        walletAction: () => {
-          window.open(url, "_blank");
-        },
-      })),
-  ];
-
-  const buttons = wallets.map((wallet, index) => (
-    <Button
-      key={index}
-      w="100%"
-      minH="4rem"
-      bg="brand.darkBrown"
-      p="6"
-      mb="4"
-      borderRadius="xl"
-      transition="0.2s all"
-      _hover={{
-        bg: "brand.darkerBrown",
-        color: "white",
-      }}
-      onClick={() => {
-        onClose();
-        wallet.walletAction();
-      }}
-    >
-      <Flex w="100%" align="center">
-        <Text>{wallet.name}</Text>
-        <Spacer />
-        <Image src={wallet.icon} htmlWidth="24" alt="" />
-      </Flex>
-    </Button>
-  ));
+  const setKeplrMemo = useCallback(() => {
+    setKeplrAndConnect()
+    onCloseModal()
+  }, [activeWallet, chainId, network])
 
   return (
-    <Button
-      type="button"
-      bg="brand.darkBrown"
-      color="white"
-      py="2"
-      px="4"
-      borderRadius="full"
-      _focus={{
-        outline: "none",
-        boxShadow: "none",
-      }}
-      _hover={{
-        bg: "brand.darkerBrown",
-      }}
-      onClick={onOpen}
-    >
-      <HStack spacing="3">
-        <TerraIcon width="1.25rem" height="1.25rem" />
-        <Text fontSize="md">Connect your wallet</Text>
+    <Button variant="wallet" onClick={() => setKeplrMemo()} colorScheme="black">
+      <HStack justify="space-between" width="full">
+        <Text>Keplr Wallet</Text>
+        <KeplrWalletIcon />
       </HStack>
-      <ModalWrapper isOpen={isOpen} onClose={onClose} title="Connect to a wallet">
-        {buttons}
-      </ModalWrapper>
     </Button>
-  );
-};
+  )
+}
 
-export default WalletConnectButton;
+export default KeplrConnectButton
